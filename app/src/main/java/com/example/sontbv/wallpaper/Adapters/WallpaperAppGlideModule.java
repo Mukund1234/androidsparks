0d850cf857fff885df74aab4cf31460a2f7022be
Mukund1234 <mukundshil1234@gmail.com>
2019-04-30 01:08:14 +0530

    android2

---
+++ app/src/main/java/com/example/sontbv/wallpaper/Adapters/WallpaperAppGlideModule.java
@@ -0,0 +1,10 @@
+package com.example.sontbv.wallpaper.Adapters;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+
+
+@GlideModule
+public class WallpaperAppGlideModule extends AppGlideModule {
+}
